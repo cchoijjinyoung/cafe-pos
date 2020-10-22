163a3daaf3174bbeef4792132cd5fb811dba0a16
bitcamp <kimst0020>
2020-10-22 12:45:16 +0900

    ㅇ

--- pos-server/src/main/java/com/pos/pms/listener/DataHandlerListener.java
+++ pos-server/src/main/java/com/pos/pms/listener/DataHandlerListener.java
@@ -12,54 +12,68 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import com.eomcs.context.ApplicationContextListener;
-import com.eomcs.pms.domain.Board;
-import com.eomcs.pms.domain.Member;
-import com.eomcs.pms.domain.Project;
-import com.eomcs.pms.domain.Task;
 import com.google.gson.Gson;
+import com.pos.context.ApplicationContextListener;
+import com.pos.pms.domain.Bakery;
+import com.pos.pms.domain.ColdCoffee;
+import com.pos.pms.domain.Employee;
+import com.pos.pms.domain.HotCoffee;
+import com.pos.pms.domain.IceCreamCone;
+import com.pos.pms.domain.IceCreamCup;
 
 // 게시물, 회원, 프로젝트, 작업 데이터를 파일에서 로딩하고 파일로 저장하는 일을 한다.
 public class DataHandlerListener implements ApplicationContextListener {
 
-  List<Board> boardList = new ArrayList<>();
-  File boardFile = new File("./board.json"); // 게시글을 저장할 파일 정보
+  List<HotCoffee> hotCoffeeList = new ArrayList<>();
+  File hotCoffeeFile = new File("./hotCoffee.json"); // 게시글을 저장할 파일 정보
 
-  List<Member> memberList = new LinkedList<>();
-  File memberFile = new File("./member.json"); // 회원을 저장할 파일 정보
+  List<ColdCoffee> coldCoffeeList = new LinkedList<>();
+  File coldCoffeeFile = new File("./coldCoffee.json"); // 회원을 저장할 파일 정보
 
-  List<Project> projectList = new LinkedList<>();
-  File projectFile = new File("./project.json"); // 프로젝트를 저장할 파일 정보
+  List<Bakery> bakeryList = new LinkedList<>();
+  File bakeryFile = new File("./bakery.json"); // 프로젝트를 저장할 파일 정보
 
-  List<Task> taskList = new ArrayList<>();
-  File taskFile = new File("./task.json"); // 작업을 저장할 파일 정보
+  List<Employee> employeeList = new ArrayList<>();
+  File employeeFile = new File("./employee.json"); // 작업을 저장할 파일 정보
+
+  List<IceCreamCone> iceCreamConeList = new LinkedList<>();
+  File iceCreamConeFile = new File("./iceCreamCone.json"); // 프로젝트를 저장할 파일 정보
+
+  List<IceCreamCup> iceCreamCupList = new ArrayList<>();
+  File iceCreamCupFile = new File("./iceCreamCup.json"); // 작업을 저장할 파일 정보
 
   @Override
   public void contextInitialized(Map<String,Object> context) {
     // 애플리케이션의 서비스가 시작되면 먼저 파일에서 데이터를 로딩한다.
     // 파일에서 데이터 로딩
-    loadData(boardList, boardFile, Board[].class);
-    loadData(memberList, memberFile, Member[].class);
-    loadData(projectList, projectFile, Project[].class);
-    loadData(taskList, taskFile, Task[].class);
+    loadData(hotCoffeeList, hotCoffeeFile, HotCoffee[].class);
+    loadData(coldCoffeeList, coldCoffeeFile, ColdCoffee[].class);
+    loadData(bakeryList, bakeryFile, Bakery[].class);
+    loadData(employeeList, employeeFile, Employee[].class);
+    loadData(iceCreamConeList, iceCreamConeFile, IceCreamCone[].class);
+    loadData(iceCreamCupList, iceCreamCupFile, IceCreamCup[].class);
 
     // 옵저버가 파일에서 데이터(게시글,회원,프로젝트,작업)를 읽어
     // List 컬렉션에 저장한 다음,
     // 발행자(App 객체)가 사용할 수 있도록 맵 객체에 담아서 공유한다.
-    context.put("boardList", boardList);
-    context.put("memberList", memberList);
-    context.put("projectList", projectList);
-    context.put("taskList", taskList);
+    context.put("hotCoffeeList", hotCoffeeList);
+    context.put("coldCoffeeList", coldCoffeeList);
+    context.put("bakeryList", bakeryList);
+    context.put("employeeList", employeeList);
+    context.put("iceCreamConeList", iceCreamConeList);
+    context.put("iceCreamCupList", iceCreamCupList);
   }
 
   @Override
   public void contextDestroyed(Map<String,Object> context) {
     // 애플리케이션 서비스가 종료되면 컬렉션에 보관된 객체를 파일에 저장한다.
     // 데이터를 파일에 저장
-    saveData(boardList, boardFile);
-    saveData(memberList, memberFile);
-    saveData(projectList, projectFile);
-    saveData(taskList, taskFile);
+    saveData(hotCoffeeList, hotCoffeeFile);
+    saveData(coldCoffeeList, coldCoffeeFile);
+    saveData(bakeryList, bakeryFile);
+    saveData(employeeList, employeeFile);
+    saveData(iceCreamConeList, iceCreamConeFile);
+    saveData(iceCreamCupList, iceCreamCupFile);
   }
 
   private <T> void loadData(
