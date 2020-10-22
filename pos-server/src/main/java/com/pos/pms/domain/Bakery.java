aa26c08cdb34a04b57bb78e30e931c68f6e3a506
bitcamp <kimst0020>
2020-10-22 10:58:48 +0900

    도메인 추가

---
+++ pos-server/src/main/java/com/pos/pms/domain/Bakery.java
@@ -0,0 +1,29 @@
+package com.pos.pms.domain;
+
+public class Bakery {
+  private int price;
+  private String name;
+  private String type;
+
+  public int getPrice() {
+    return price;
+  }
+  public void setPrice(int price) {
+    this.price = price;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getType() {
+    return type;
+  }
+  public void setType(String type) {
+    this.type = type;
+  }
+
+
+
+}
